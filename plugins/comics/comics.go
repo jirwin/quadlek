d17e12933471ab0bfb0898e39f9080b9cae7d085
Justin Gallardo <me@jirw.in>
2018-04-08 02:56:16 -0700

    .

--- plugins/comics/comics.go
+++ plugins/comics/comics.go
@@ -5,7 +5,11 @@ package comics
 import (
 	"context"
 
+	"fmt"
+	"strings"
+
 	log "github.com/Sirupsen/logrus"
+	"github.com/golang/protobuf/proto"
 	"github.com/jirwin/quadlek/quadlek"
 )
 
@@ -17,10 +21,43 @@ func comicCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) {
 	for {
 		select {
 		case cmdMsg := <-cmdChannel:
-			cmdMsg.Command.Reply() <- &quadlek.CommandResp{
-				Text:      "got a comic request",
-				InChannel: false,
+			cmdMsg.Command.Reply() <- nil
+
+			if cmdMsg.Command.Text != "" {
+				split := strings.Split(cmdMsg.Command.Text, " ")
+				switch split[0] {
+				case "load":
+					if len(split) != 2 {
+						cmdMsg.Bot.RespondToSlashCommand(cmdMsg.Command.ResponseUrl, &quadlek.CommandResp{
+							Text:      "You must provide the url of a template to load.",
+							InChannel: false,
+						})
+					}
+					err := cmdMsg.Store.GetAndUpdate("template", func(templatesProto []byte) ([]byte, error) {
+						templates := &Templates{}
+						err := proto.Unmarshal(templatesProto, templates)
+						if err != nil {
+							return nil, err
+						}
+
+						templates.Urls = append(templates.Urls, split[1])
+
+						updatedTemplates, err := proto.Marshal(templates)
+						if err != nil {
+							return nil, err
+						}
+
+						return updatedTemplates, nil
+					})
+					if err != nil {
+						cmdMsg.Bot.RespondToSlashCommand(cmdMsg.Command.ResponseUrl, &quadlek.CommandResp{
+							Text:      fmt.Sprintf("error adding template: %s", err.Error()),
+							InChannel: false,
+						})
+					}
+				}
 			}
+			cmdMsg.Command.Reply() <- nil
 
 		case <-ctx.Done():
 			log.Info("Exiting comic command.")
