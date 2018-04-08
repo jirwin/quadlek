710c0b7b8bfbfb6ed0fadb3eec3a3dfafa9bbd05
Justin Gallardo <me@jirw.in>
2018-04-08 03:04:23 -0700

    .

--- plugins/comics/comics.go
+++ plugins/comics/comics.go
@@ -17,6 +17,35 @@ var (
 	clientId string
 )
 
+func addComicTemplate(templateUrl string, cmdMsg *quadlek.CommandMsg) error {
+	err := cmdMsg.Store.GetAndUpdate("template", func(templatesProto []byte) ([]byte, error) {
+		templates := &Templates{}
+		err := proto.Unmarshal(templatesProto, templates)
+		if err != nil {
+			return nil, err
+		}
+
+		templates.Urls = append(templates.Urls, templateUrl)
+
+		updatedTemplates, err := proto.Marshal(templates)
+		if err != nil {
+			return nil, err
+		}
+
+		return updatedTemplates, nil
+	})
+	if err != nil {
+		return err
+	}
+
+	cmdMsg.Bot.RespondToSlashCommand(cmdMsg.Command.ResponseUrl, &quadlek.CommandResp{
+		Text:      "Successfully added template " + templateUrl,
+		InChannel: false,
+	})
+
+	return nil
+}
+
 func comicCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) {
 	for {
 		select {
@@ -32,32 +61,25 @@ func comicCommand(ctx context.Context, cmdChannel <-chan *quadlek.CommandMsg) {
 							Text:      "You must provide the url of a template to load.",
 							InChannel: false,
 						})
+						continue
 					}
-					err := cmdMsg.Store.GetAndUpdate("template", func(templatesProto []byte) ([]byte, error) {
-						templates := &Templates{}
-						err := proto.Unmarshal(templatesProto, templates)
-						if err != nil {
-							return nil, err
-						}
-
-						templates.Urls = append(templates.Urls, split[1])
-
-						updatedTemplates, err := proto.Marshal(templates)
-						if err != nil {
-							return nil, err
-						}
-
-						return updatedTemplates, nil
-					})
+					err := addComicTemplate(split[1], cmdMsg)
 					if err != nil {
 						cmdMsg.Bot.RespondToSlashCommand(cmdMsg.Command.ResponseUrl, &quadlek.CommandResp{
 							Text:      fmt.Sprintf("error adding template: %s", err.Error()),
 							InChannel: false,
 						})
+						continue
 					}
+
+					cmdMsg.Bot.RespondToSlashCommand(cmdMsg.Command.ResponseUrl, &quadlek.CommandResp{
+						Text:      "Successfully added template " + split[1],
+						InChannel: false,
+					})
 				}
+
+				continue
 			}
-			cmdMsg.Command.Reply() <- nil
 
 		case <-ctx.Done():
 			log.Info("Exiting comic command.")
