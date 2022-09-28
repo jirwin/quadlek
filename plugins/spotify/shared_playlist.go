ed6e15ee284af77922d481b55a474e569b1d6fa2
Justin Gallardo <justin.gallardo@conductorone.com>
2022-09-28 11:11:56 -0700

    Allow shared playlists to be configured via the env

--- plugins/spotify/shared_playlist.go
+++ plugins/spotify/shared_playlist.go
@@ -2,24 +2,42 @@ package spotify
 
 import (
 	"context"
+	"os"
+	"regexp"
+
 	v1 "github.com/jirwin/quadlek/pb/quadlek/plugins/spotify/v1"
 
 	"go.uber.org/zap"
-
-	"regexp"
 
 	"github.com/jirwin/quadlek/quadlek"
 	"github.com/zmb3/spotify"
 	"google.golang.org/protobuf/proto"
 )
 
-// FIXME: These shouldn't have to be hardcoded
 const (
 	sharedPlaylist     = "2oeMx9gAl3fx2qThl11jt1"
 	sharedPlaylistUser = "U0RL53ETW"
 )
 
 var spotifyTrackRegex = regexp.MustCompile(`spotify:track:(\w+)\b`)
+
+func getSharedPlaylist() string {
+	playlist := os.Getenv("SPOTIFY_SHARED_PLAYLIST")
+	if playlist != "" {
+		return playlist
+	}
+
+	return sharedPlaylist
+}
+
+func getSharedPlaylistUser() string {
+	playlistUser := os.Getenv("SPOTIFY_SHARED_PLAYLIST_USER")
+	if playlistUser != "" {
+		return playlistUser
+	}
+
+	return sharedPlaylistUser
+}
 
 func saveSongsHook(ctx context.Context, hookChannel <-chan *quadlek.HookMsg) {
 	for {
@@ -35,7 +53,7 @@ func saveSongsHook(ctx context.Context, hookChannel <-chan *quadlek.HookMsg) {
 				tracks = append(tracks, spotify.ID(match[1]))
 			}
 
-			err := hookMsg.Store.Get("authtoken-"+sharedPlaylistUser, func(val []byte) error {
+			err := hookMsg.Store.Get("authtoken-"+getSharedPlaylistUser(), func(val []byte) error {
 				authToken := &v1.AuthToken{}
 				err := proto.Unmarshal(val, authToken)
 				if err != nil {
@@ -51,7 +69,7 @@ func saveSongsHook(ctx context.Context, hookChannel <-chan *quadlek.HookMsg) {
 					zap.L().Info("detected a song, but need to reauth before it can be added.")
 				}
 
-				snapshotId, err := client.AddTracksToPlaylist(sharedPlaylist, tracks...)
+				snapshotId, err := client.AddTracksToPlaylist(spotify.ID(getSharedPlaylist()), tracks...)
 				if err != nil {
 					zap.L().Error("error adding tracks to shared playlist", zap.Error(err))
 					return err
